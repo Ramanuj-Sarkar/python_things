fa497ca99d59054cf1c343238406468e10bb8fb5
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2021-10-30 11:47:18 -0700

    Update run_pig.py

--- run_pig.py
+++ run_pig.py
@@ -1,7 +1,10 @@
 def run_pig(code):
     assert type(code) is str, 'ERROR: Code must be a string.'
     assert 'PIG' in code, 'ERROR: Code must contain "PIG".'
-    file_name, file_text = code.split('PIG')
+    file_name, file_text = code.split('PIG',1)
     file = open(f'{file_name}.txt', 'w')
     file.write(file_text)
     file.close()
+
+if __name__ == '__main__':
+    run_pig(input())
