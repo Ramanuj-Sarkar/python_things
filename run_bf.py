97a19678abe2fa1fa23a83476535ae16f7895ff8
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2021-11-26 03:02:20 -0800

    Update run_bf.py
    I based this on something else, so I had some issues that I wanted to fix now.

--- run_bf.py
+++ run_bf.py
@@ -1,18 +1,23 @@
 def run_bf(code: str) -> None:
     pointer=0   # for instructions
     location=0  # for tape
-    tape=[0]*999
+    tape=[0]
     while pointer < len(code):
         if code[pointer] == '>':
             location += 1
+            if location == len(tape):
+                tape.append(0)
         elif code[pointer] == '<':
             location -= 1
+            if location < 0:
+                tape = [0] + tape
+                location += 1
         elif code[pointer] == '+':
             tape[location] += 1
         elif code[pointer] == '-':
             tape[location] -= 1
         elif code[pointer] == '.':
-            print(chr(tape[location]))
+            print(chr(tape[location]),end='')
         elif code[pointer] == ',':
             tape[location] = ord(input())
         elif code[pointer] == '[':
