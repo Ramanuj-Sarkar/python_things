c11138f2e5b38306242152aa7a572152cdc7e567
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2021-12-10 17:32:27 -0800

    Update run_bf.py
    Fixed bugs which led to crashes and added error message for unbalanced brackets.

--- run_bf.py
+++ run_bf.py
@@ -1,7 +1,19 @@
-def run_bf(code: str) -> None:
-    pointer=0   # for instructions
-    location=0  # for tape
-    tape=[0]
+ef run_bf(code: str) -> None:
+    pointer = 0     # for instructions
+    location = 0    # for tape
+    tape = [0]
+    input_string = chr(10)  # allows multiple inputs to happen easily
+
+    # checks balance
+    balance = 0
+    for char in code:
+        if char == '[':
+            balance += 1
+        elif char == ']':
+            balance -= 1
+        assert balance >= 0, 'unmatched ]'
+    assert balance == 0, 'unmatched ['
+
     while pointer < len(code):
         if code[pointer] == '>':
             location += 1
@@ -19,7 +31,11 @@ def run_bf(code: str) -> None:
         elif code[pointer] == '.':
             print(chr(tape[location]),end='')
         elif code[pointer] == ',':
-            tape[location] = ord(input())
+            if input_string == chr(10):
+                input_string = input(">>") + chr(10)
+            if len(input_string) > 0 and ord(input_string[0]) != chr(10):
+                tape[location] = ord(input_string[0])
+            input_string = input_string[1:]
         elif code[pointer] == '[':
             if tape[location] == 0:
                 nest_counter = 0
