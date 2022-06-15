c6f3359aec37a58a6571450627f742f8f36bf166
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2022-06-14 19:00:16 -0700

    Update binary_coin_flip.py

--- binary_coin_flip.py
+++ binary_coin_flip.py
@@ -1,10 +1,7 @@
-# takes in number
-# returns a list of groupings
-# of coin flips which can be used
-# to find a digit in range(0,number)
-# if the coin flips are modeled as positive binary digits
+# With n coins, you can add a number from 0 to 2^n - 1.
 def binary_coin_flip(numbers: int):
     grouped_coins = []
+    assert numbers >= 0, 'A negative number is not a valid input.'
     while numbers != 0:
         coins = 1
         while ((2 ** (coins + 1)) - 1) <= numbers:
@@ -12,3 +9,18 @@ def binary_coin_flip(numbers: int):
         numbers -= (2 ** coins) - 1
         grouped_coins.append(coins)
     return grouped_coins
+
+
+if __name__ == "__main__":
+    num = int(input('Enter a number:'))
+    print(binary_coin_flip(num))
+    print("Using binary numbers, you get: ")
+    coins_dict = dict()
+    for x in range(num):
+        coins = sum(binary_coin_flip(x))
+        if coins not in coins_dict:
+            coins_dict[coins] = set()
+        coins_dict[coins].add(x)
+
+    for coins_used in coins_dict:
+        print(f"Use {coins_used} numbers: {coins_dict[coins_used]}")
