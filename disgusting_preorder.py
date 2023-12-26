bfca0b41e60fc63a91d5e4db3a1539ccb6f65281
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2023-12-26 12:58:25 -0800

    Update disgusting_preorder.py

--- disgusting_preorder.py
+++ disgusting_preorder.py
@@ -1,57 +1,36 @@
-# This exists so I could represent a linear string as a tree
-# and represent a tree as a linear string
-# in terms of which order the characters would be seen.
-# It is disgusting, especially make_preorder.
-
-linear_number = 0
+# The preorder used to be worse, requiring a global variable.
+# It no longer requires that, but it is still a little disgusting, isn't it?
 
 # This takes a linear string
-# and turns it into a tree which would be read
-# the same way as the linear string using pre-order.
-def make_preorder(linear, treeshape=[''], treeshape_number=1):
-    if treeshape == ['']:
-        treeshape = [''] * len(linear)
-    global linear_number
-    treeshape[treeshape_number-1] = linear[linear_number]
-    if treeshape_number * 2 <= len(linear):
-        # if input() != '': return
-        linear_number += 1
-        make_preorder(linear, treeshape, treeshape_number * 2)
-    if treeshape_number * 2 + 1 <= len(linear):
-        # if input() != '': return
-        linear_number += 1
-        make_preorder(linear, treeshape, treeshape_number * 2 + 1)
-    return ''.join(treeshape)
+# and turns it into the linear representation of
+# a tree which would be read the same way as the linear string using pre-order.
+def make_preorder(linear, linear_num=0, tree_num=1, final=True):
+    starting_num = linear_num
+    tree_correlations = {tree_num: linear[linear_num]}
+    if tree_num * 2 <= len(linear):
+        starting_num += 1
+        temp, more_correlations = make_preorder(linear, starting_num, tree_num * 2, False)
+        starting_num += temp
+        tree_correlations |= more_correlations
+    if tree_num * 2 + 1 <= len(linear):
+        starting_num += 1
+        temp, more_correlations = make_preorder(linear, starting_num, tree_num * 2 + 1, False)
+        starting_num += temp
+        tree_correlations |= more_correlations
 
-# This takes in a string representing the tree
+    if final:
+        return ''.join([string for _, string in sorted(tree_correlations.items())])
+    else:
+        return starting_num - linear_num, tree_correlations
+
+
+# This takes in a string representing a tree
 # and reads it using pre-order
 # thereby resurrecting the original string.
-def decode_preorder(treestring, treestring_number=1):
-    output_string = treestring[treestring_number-1]
-    if treestring_number * 2 <= len(treestring):
-        # if input() != '': return
-        output_string += decode_preorder(treestring, treestring_number * 2)
-    if treestring_number * 2 + 1 <= len(treestring):
-        # if input() != '': return
-        output_string += decode_preorder(treestring, treestring_number * 2 + 1)
+def decode_preorder(tree_string, tree_number=1):
+    output_string = tree_string[tree_number-1]
+    if tree_number * 2 <= len(tree_string):
+        output_string += decode_preorder(tree_string, tree_number * 2)
+    if tree_number * 2 + 1 <= len(tree_string):
+        output_string += decode_preorder(tree_string, tree_number * 2 + 1)
     return output_string
-
-ultimate_machine = 'ABCDEFGHIJKLMNOP'
-
-linear_number = 0
-
-for maxo in range(1,len(ultimate_machine)+1):
-    linear_number = 0
-    truth_machine = ultimate_machine[:maxo]
-    things = [''] * maxo
-    preodered_string = make_preorder(truth_machine)
-    print(preodered_string)
-    print(decode_preorder(preodered_string))
-
-linear_number = 0
-print()
-code = '++++++[<++++++++^>++++++++^-],<[-^-<]^[-[<>-^]>+[.]]>[.^]'
-trench = [''] * len(code)
-finished = make_preorder(code)
-print(finished)
-print(decode_preorder(finished))
