dd532d9902fe8dc6cfcdbb5511eec3bdf9eba3b7
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-08 23:45:18 -0700

    Update max_words.py
    Why do I do these things?

--- max_words.py
+++ max_words.py
@@ -1,12 +1,11 @@
 # syllables is the number of possible syllables
 # length is the maximum number of syllables in a word
 def max_words(syllables,length):
-    assert syllables >= 1, "There has to be at least 1 valid syllable."
-    assert length >= 1, "Words have to be at least 1 syllable long."
-    total = 0
-    for x in range(1,length+1):
-        total += syllables ** x
-    print(total)
+    if syllables < 1:
+        raise ValueError("There has to be at least 1 valid syllable.")
+    if length < 1:
+        raise ValueError("Words have to be at least 1 syllable long.")
+    return sum(syllables ** x for x in range(1,length+1))
 
 
 if __name__ == '__main__':
